bd7dec09d275ba51e7bca8e745d0ab089a900f1d
mddon <mmrealworld@yahoo.com>
2024-06-22 14:19:18 -0700

    update

--- student_management/student_management/urls.py
+++ student_management/student_management/urls.py
@@ -20,6 +20,5 @@ from students import views as student_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('students/', include('students.urls')),
-    path('', student_views.index, name='index'),
+    path('', include('students.urls')),
 ]
